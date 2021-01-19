2f2fa08e709fa7e5f9a849f3a9142f0963c75464
relotnek <kentoler@gmail.com>
2021-01-19 17:59:17 -0500

    removed vars from main for separation

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-variable "ports_to_open" {
-    type = list
-    default = [443,22,8834]
-}
-
-variable "approved_key_name" {
-  type = string
-}
-
-variable "approved_ips" {
-  type = string
-  default = "0.0.0.0/0"
-}
-
 resource "aws_security_group" "nessus_rules" {
   name        = "nessus_rules"
   description = "Ports Required for Nessus Vulnerability Scanner"
